5b88e1d88736f52aa2dec5e4da04e6a53f38201e
Maiwa <maiwa0126@gmail.com>
2022-08-17 13:30:15 +0800

    monzzle2.0

---
+++ monzzle2.0/js/menu.js
@@ -0,0 +1,68 @@
+let pages = ['首頁', '關於我們', '專題介紹', '妖怪選單', '小遊戲', '商城']
+let navs = [];
+
+$(document).ready(() => {
+    $('.menu .left .navs .nav').css('--myScale', 'scale(0)');
+    menuChange();
+});
+
+$('.menu .left .navs .nav a').mouseover(() => {
+    $('.menu .left .navs .nav a').css('color', '#a8877d');
+});
+$('.menu .left .navs .nav a').mouseout(() => {
+    $('.menu .left .navs .nav a').css('color', '#513A35');
+});
+
+document.getElementById('menu-btn').addEventListener('mousemove', function (e) {
+            let winWidth = $(window).width();
+            if (winWidth > 900) {
+                const position = document.getElementById('menu-btn').getBoundingClientRect();
+                const x = e.pageX - position.left - position.width / 2;
+                const y = e.pageY - position.top - position.height / 2;
+
+                document.getElementById('menu-border').style.transform = 'scale(1)';
+
+                document.getElementById('menu-btn').style.transform = "translate(" + x * 0.2 + "px," + y * 0.4 + "px)";
+                document.getElementById('menu-border').style.transform = "translate(" + x * 0.5 + "px," + y * 0.7 + "px)";
+            }
+        });
+
+        document.getElementById('menu-btn').addEventListener('mouseout', function (e) {
+            document.getElementById('menu-btn').style.transform = "translate(0px,0px)";
+            document.getElementById('menu-border').style.transform = "translate(0px,0px)"
+            document.getElementById('menu-border').style.transform = 'scale(0)';
+        });
+
+        function currentPage() {
+            let pageTitle = document.title;
+            for (let i = 0; i < pages.length; i++) {
+                console.log('nope');
+                if (pageTitle == pages[i]) {
+                    return i;
+                }
+            }
+        };
+
+        let isMenuOpen = false;
+        $('#menu-btn').click(() => {
+
+            if (!isMenuOpen) {
+                $('.menu').css('transform', 'translateY(0)');
+                $('#btn').removeClass('fa-angle-down');
+                $('#btn').addClass('fa-xmark');
+                isMenuOpen = true;
+            } else if (isMenuOpen) {
+                $('.menu').css('transform', 'translateY(-100%)');
+                $('#btn').removeClass('fa-xmark');
+                $('#btn').addClass('fa-angle-down');
+                isMenuOpen = false;
+            } else {
+                alert('菜單錯誤boolen!');
+            }
+
+        });
+
+        function menuChange() {
+            let i = currentPage() + 1;
+            $(`.nav-${i}`).css('--myScale', 'scale(1)');
+        }
