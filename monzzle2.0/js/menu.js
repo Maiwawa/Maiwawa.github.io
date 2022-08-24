d2af411763790488c0e0e23e00c099afed934930
Maiwa <maiwa0126@gmail.com>
2022-08-24 11:21:13 +0800

    monzzle

--- monzzle2.0/js/menu.js
+++ monzzle2.0/js/menu.js
@@ -14,55 +14,59 @@ $('.menu .left .navs .nav a').mouseout(() => {
 });
 
 document.getElementById('menu-btn').addEventListener('mousemove', function (e) {
-            let winWidth = $(window).width();
-            if (winWidth > 900) {
-                const position = document.getElementById('menu-btn').getBoundingClientRect();
-                const x = e.pageX - position.left - position.width / 2;
-                const y = e.pageY - position.top - position.height / 2;
+    let winWidth = $(window).width();
+    if (winWidth > 800) {
+        const position = document.getElementById('menu-btn').getBoundingClientRect();
+        const x = e.pageX - position.left - position.width / 2;
+        const y = e.pageY - position.top - position.height / 2;
 
-                document.getElementById('menu-border').style.transform = 'scale(1)';
+        document.getElementById('menu-border').style.transform = 'scale(1)';
 
-                document.getElementById('menu-btn').style.transform = "translate(" + x * 0.2 + "px," + y * 0.4 + "px)";
-                document.getElementById('menu-border').style.transform = "translate(" + x * 0.5 + "px," + y * 0.7 + "px)";
-            }
-        });
+        document.getElementById('menu-btn').style.transform = "translate(" + x * 0.2 + "px," + y * 0.4 + "px)";
+        document.getElementById('menu-border').style.transform = "translate(" + x * 0.5 + "px," + y * 0.7 + "px)";
+    }
+});
 
-        document.getElementById('menu-btn').addEventListener('mouseout', function (e) {
-            document.getElementById('menu-btn').style.transform = "translate(0px,0px)";
-            document.getElementById('menu-border').style.transform = "translate(0px,0px)"
-            document.getElementById('menu-border').style.transform = 'scale(0)';
-        });
+document.getElementById('menu-btn').addEventListener('mouseout', function (e) {
+    document.getElementById('menu-btn').style.transform = "translate(0px,0px)";
+    document.getElementById('menu-border').style.transform = "translate(0px,0px)"
+    document.getElementById('menu-border').style.transform = 'scale(0)';
+});
 
-        function currentPage() {
-            let pageTitle = document.title;
-            for (let i = 0; i < pages.length; i++) {
-                console.log('nope');
-                if (pageTitle == pages[i]) {
-                    return i;
-                }
-            }
-        };
-
-        let isMenuOpen = false;
-        $('#menu-btn').click(() => {
-
-            if (!isMenuOpen) {
-                $('.menu').css('transform', 'translateY(0)');
-                $('#btn').removeClass('fa-angle-down');
-                $('#btn').addClass('fa-xmark');
-                isMenuOpen = true;
-            } else if (isMenuOpen) {
-                $('.menu').css('transform', 'translateY(-100%)');
-                $('#btn').removeClass('fa-xmark');
-                $('#btn').addClass('fa-angle-down');
-                isMenuOpen = false;
-            } else {
-                alert('菜單錯誤boolen!');
-            }
-
-        });
-
-        function menuChange() {
-            let i = currentPage() + 1;
-            $(`.nav-${i}`).css('--myScale', 'scale(1)');
+function currentPage() {
+    let pageTitle = document.title;
+    for (let i = 0; i < pages.length; i++) {
+        console.log('nope');
+        if (pageTitle == pages[i]) {
+            return i;
         }
+    }
+};
+
+let isMenuOpen = false;
+$('#menu-btn').click(() => {
+    let winWidth = $(window).width();
+    if (winWidth > 800) {
+        
+    }
+
+    if (!isMenuOpen) {
+        $('.menu').css('transform', 'translateY(0)');
+        $('#btn').removeClass('fa-angle-down');
+        $('#btn').addClass('fa-xmark');
+        isMenuOpen = true;
+    } else if (isMenuOpen) {
+        $('.menu').css('transform', 'translateY(-100%)');
+        $('#btn').removeClass('fa-xmark');
+        $('#btn').addClass('fa-angle-down');
+        isMenuOpen = false;
+    } else {
+        alert('菜單錯誤boolen!');
+    }
+
+});
+
+function menuChange() {
+    let i = currentPage() + 1;
+    $(`.nav-${i}`).css('--myScale', 'scale(1)');
+}
